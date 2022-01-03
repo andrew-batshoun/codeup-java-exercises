30d16f92a2512bc1891e62c11fc73b0a1048b676
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2022-01-03 14:41:33 -0600

    started extra exercise fundamentals

---
+++ src/ExtraExercisesFundamentals.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ExtraExercisesFundamentals {
+    private static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //Write a program in Java to read 10 numbers from keyboard and find their sum and average
+//        ArrayList<Integer> keyBoardNum = new ArrayList<>();
+//        keyBoardNum.add(1);
+//        keyBoardNum.add(2);
+//        keyBoardNum.add(3);
+//        keyBoardNum.add(4);
+//        keyBoardNum.add(5);
+//        keyBoardNum.add(6);
+//        keyBoardNum.add(7);
+//        keyBoardNum.add(8);
+//        keyBoardNum.add(9);
+//        keyBoardNum.add(0);
+//        int sum = 0;
+//        for (int i = 0; i < keyBoardNum.size(); i++) {
+//            sum += keyBoardNum.get(i);
+//        }
+//        double average = sum / keyBoardNum.size();
+//        System.out.println("The sum of 10 numbers is: " + sum);
+//        System.out.println("The Average is: " + average);
+        //Write a program in Java to display the cube of the number upto given an integer. Read the number using the Scanner class.
+//        boolean confirm;
+//        do {
+//            System.out.println("Choose a number: ");
+//            int choosenNum = scan.nextInt();
+//            for (int i = 1; i <= choosenNum; i++) {
+//                int cubed = i * i * i;
+//                System.out.printf("Number is : %d and cube of the %d is : %d%n", i, i, cubed);
+//            }
+//            System.out.println("Would you like to choose another number? [y/n]");
+//            System.out.println();
+//            String confirmAnswer = scan.next();
+//            confirm = confirmAnswer.equalsIgnoreCase("y");
+//        }while (confirm);
+
+        //Write a program in Java to display the n terms of odd natural number and their sum. Read the odd number using the Scanner class
+
+
+    }
+}
