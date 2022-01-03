659b88f8d2601fff543c57a9dac56400c5c2ec0a
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2022-01-03 15:01:41 -0600

    updated

--- src/Bob.java
+++ src/Bob.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean later;
+        boolean later = true;
         do {
             System.out.println("What's up?");
             String convo = scanner.nextLine();
@@ -15,15 +15,17 @@ public class Bob {
                 System.out.println("Whoa, chill out!");
             } else if (convo.equals("")) {
                 System.out.println("Fine. Be that way!");
+            } else if (convo.contains("later")) {
+                System.out.println("Later");
+                later = false;
+
             } else {
                 System.out.println("Whatever.");
             }
-            System.out.println("Anything else?");
-            String answer = scanner.nextLine();
-            later = answer.equalsIgnoreCase("yes");
 
-        }while(later);
 
-        }
+        } while (later);
+
     }
+}
 
