f2fe0a38f0bec6103602f70693b99dec1bf51d92
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2022-01-04 12:13:02 -0600

    completed method exercise 1-4

--- src/MethodExercise.java
+++ src/MethodExercise.java
@@ -1,3 +1,4 @@
+import java.lang.Math;
 import java.util.Scanner;
 
 public class MethodExercise {
@@ -30,42 +31,84 @@ public class MethodExercise {
     }
 
 
-
     //Create a method that validates that user input is in a certain range
 
     public static int getInteger(int min, int max) {
-            System.out.printf("Enter a number between %d and %d: ", min, max);
-            int userInput = scan.nextInt();
-            if (userInput >= 1 && userInput <= 10) {
-                System.out.println("Your number is with in range.");
-                return userInput;
-            } else {
-                System.out.println("That is not in range.");
-                return getInteger(min,max);
-            }
+        System.out.printf("Enter a number between %d and %d: ", min, max);
+        int userInput = scan.nextInt();
+        if (userInput >= 1 && userInput <= 10) {
+            System.out.println("Your number is with in range.");
+            return userInput;
+        } else {
+            System.out.println("That is not in range.");
+            return getInteger(min, max);
         }
-//    public static int factorial() {
-//        System.out.println("Choose a number from 1 to 10.");
-//        int userNum = scan.nextInt();
-//        long fact = 1;
-//        for (int i = 1; i <= userNum; i++) {
-//            fact = fact * i;
-//
-//        }
-//    }
+    }
 
+    public static long factorial(int n) {
+        long output = 1;
+
+        for (int i = 1; i <= n; i++) {
+            output *= i;
+
+            System.out.println("The factorial of " +i + "! = " + output);
+        }
+        return output;
+
+    }
+
+    //    public static long factorial(long n) {
+//        if(n<=1){
+//            return 1;
+//        }else{
+//            return n * factorial(n -1);
+//        }
+//
+//
+//    }
+    public static void rollDice(int num) {
+        int dice1 = (int) (Math.random() * (num - 1) + 1);
+        int dice2 = (int) (Math.random() * (num - 1) + 1);
+        System.out.printf("Dice 1 rolled: %d | Dice 2 rolled : %d%n", dice1, dice2);
+    }
 
 
     public static void main(String[] args) {
-        System.out.println(addition(3, 4));
-        System.out.println(subtraction(4, 3));
-        System.out.println(multiplication(3, 10));
-        System.out.println(division(4, 2));
-        System.out.println(modulus(2, 8));
-        getInteger(1,10);
-    }
+//        System.out.println(addition(3, 4));
+//        System.out.println(subtraction(4, 3));
+//        System.out.println(multiplication(3, 10));
+//        System.out.println(division(4, 2));
+//        System.out.println(modulus(2, 8));
+
+//        boolean confirm = true;
+//        while (confirm){
+//            int userNum = getInteger(1, 10);
+//            factorial(userNum);
+//            System.out.println("would you like to go again? [y/n]");
+//            String userConfirm = scan.next();
+//            confirm = userConfirm.equalsIgnoreCase("y");
+//        }
+
+
+
+//        boolean confirm = true;
+//        while (confirm) {
+//            System.out.println("How many sides?");
+//            int sides = scan.nextInt();
+//            rollDice(sides);
+//            System.out.println("would you like to go again? [y/n]");
+//            String userConfirm = scan.next();
+//            confirm = userConfirm.equalsIgnoreCase("y");
+//        }
+
 
     }
+
+}
+
+
+
+
 
 
 
